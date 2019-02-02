a18776b77eae96f3dfd190c3fe3a2a483cb065fa
astronomical-form-6016 <carltondgrandison@gmail.com>
2019-02-02 18:06:14 +0000

    Automatically backed up by Learn

--- how_old_are_you.rb
+++ how_old_are_you.rb
@@ -2,3 +2,7 @@ require_relative './current_age_for_birth.rb'
 
 puts "what year were you born?"
 birth_year = gets.to_i
+
+users_age = current_age_for_birth(birth_year)
+
+puts "You are: " + users_age.to_s + " years old."
